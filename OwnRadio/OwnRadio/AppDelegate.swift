60f39a0698cc6fcf3ad0fb1875176ff79d9a01a9
Maxym Deygin <max@dalivsoft.com>
2016-11-24 18:25:50 +0200

    Fabric added

--- OwnRadio/OwnRadio/AppDelegate.swift
+++ OwnRadio/OwnRadio/AppDelegate.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import Fabric
+import Crashlytics
+
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,7 +20,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
 		// Override point for customization after application launch.
 		let userDefaults = UserDefaults.standard
-		
+		Fabric.with([Crashlytics.self, Answers.self])
+
 		if userDefaults.object(forKey: "UUIDDevice") == nil {
 			let UUID = NSUUID().uuidString
 			userDefaults.set(UUID, forKey: "UUIDDevice")
